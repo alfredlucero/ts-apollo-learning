1ba516080b4f236cdf0bd2ee2a33077d89a4a481
alfredlucero <alfred.lucero@ucla.edu>
2022-02-16 14:12:53 -0800

    Added GraphQL Playground plugin for offline testing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { ApolloServer } from "apollo-server";
+// import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 import { schema } from "./schema";
 export const server = new ApolloServer({
   schema,
+  // To use GraphQL Playground instead of Apollo Studio (which requires an internet connection) you can use this plugin instead
+  // plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
 const port = 3000;

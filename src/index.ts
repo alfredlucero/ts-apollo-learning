5497f8e9e552183d579474b788cc5060144ad828
alfredlucero <alfred.lucero@ucla.edu>
2022-02-17 10:24:27 -0800

    Added frontend folder/apollo client

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ export const server = new ApolloServer({
   // plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
-const port = 3000;
+const port = 4000;
 
 server.listen({ port }).then(({ url }) => {
   console.log(`Server ready at ${url}`);

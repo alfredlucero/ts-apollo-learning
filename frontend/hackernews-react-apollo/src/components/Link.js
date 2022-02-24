00a362f7c3cefb6e4e1aa39d28a25fddcc966237
alfredlucero <alfred.lucero@ucla.edu>
2022-02-23 16:21:35 -0800

    Tested out update client cache stuff

--- frontend/hackernews-react-apollo/src/components/Link.js
+++ frontend/hackernews-react-apollo/src/components/Link.js
@@ -1,11 +1,82 @@
 import React from "react";
+import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
+import { useMutation, gql } from "@apollo/client";
+import { FEED_QUERY } from "./LinkList";
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: Int!) {
+    vote(linkId: $linkId) {
+      link {
+        voters {
+          id
+        }
+      }
+    }
+  }
+`;
 
 const Link = (props) => {
   const { link } = props;
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  const [vote] = useMutation(VOTE_MUTATION, {
+    variables: {
+      linkId: link.id,
+    },
+    update: (cache, { data }) => {
+      console.log("data", data);
+      const { vote } = data;
+      const { feed } = cache.readQuery({
+        query: FEED_QUERY,
+      });
+
+      const updatedLinks = feed.links.map((feedLink) => {
+        if (feedLink.id === link.id) {
+          return {
+            ...feedLink,
+            voters: [...feedLink.voters, ...vote.link.voters],
+          };
+        }
+        return feedLink;
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: updatedLinks,
+          },
+        },
+      });
+    },
+  });
+
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{props.index + 1}.</span>
+        {authToken && (
+          <div
+            className="ml1 gray f11"
+            style={{ cursor: "pointer" }}
+            onClick={vote}
+          >
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        {
+          <div className="f6 lh-copy gray">
+            {link.voters.length} votes | by{" "}
+            {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+            {timeDifferenceForDate(link.createdAt)}
+          </div>
+        }
       </div>
     </div>
   );
--- frontend/hackernews-react-apollo/src/components/LinkList.js
+++ frontend/hackernews-react-apollo/src/components/LinkList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "./Link";
 import { useQuery, gql } from "@apollo/client";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       links {
@@ -10,6 +10,14 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        description
+        postedBy {
+          id
+          name
+        }
+        voters {
+          id
+        }
       }
     }
   }
@@ -30,8 +38,8 @@ const LinkList = () => {
     <div>
       {data && (
         <>
-          {data.feed.links.map((link) => (
-            <Link key={link.id} link={link} />
+          {data.feed.links.map((link, index) => (
+            <Link key={link.id} link={link} index={index} />
           ))}
         </>
       )}
